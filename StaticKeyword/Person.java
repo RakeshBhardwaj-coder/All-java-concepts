65dbc8f9e27b9074cfc5e40662e5605c8a32f073
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2020-10-30 08:58:00 +0530

    Delete Person.java

--- StaticKeyword/Person.java
+++
@@ -1,9 +0,0 @@
-package StaticKeyword;
-
-public class Person<Breed> {
-    int age;
-    String name;
-
-    static String Breed = "Homo Sapiens";//static which use same and always
-    //in a  class, personally static is always related to the class not to object(instance).
-}
