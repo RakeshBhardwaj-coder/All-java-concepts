c398d9f0e56eb363945853118f02f08f90ebb225
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2020-10-30 08:57:12 +0530

    Delete Teacher.java

--- Inheritance/Teacher.java
+++
@@ -1,9 +0,0 @@
-package Inheritance;
-
-import javax.crypto.spec.PSource;
-
-public class Teacher extends Person {
-    public void Teach(){
-        System.out.println(name + " is teacher and he can teach.");
-    }
-}
